cd6059bf8003ad7360a61676cb783b89c3fded18
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-04-11 15:52:56 +0530

    carousal cleared

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ const Home = ({ popularProducts, discountProducts, attributes }) => {
           <div className="min-h-screen">
             <StickyCart />
             <div className="bg-white">
-              <div className="mx-auto py-5  px-3 sm:px-10">  {/* max-w-screen-2xl */}
+              <div className="mx-auto py-2">  {/* max-w-screen-2xl */}
                 <div className="flex w-full">
                   <div className="flex-shrink  lg:block w-full ">
                     <MainCarousel />
